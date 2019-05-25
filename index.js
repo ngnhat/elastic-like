59ac15b1ccddcb0034212bf86f23bc1d2b00a74c
nhat ng <ngnhatjs@gmail.com>
2019-05-25 10:57:13 +0700

    Clean code

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const { Map, Set } = require('immutable');
-const { standardTokenizer, asciiFoldingTokenizer } = require('../tokenizers');
-// const data = require('./data.json');
-
-const emptyMap = new Map();
-const emptySet = new Set();
+const { standardTokenizer, asciiFoldingTokenizer } = require('tokenizers');
 
 const analyzerMapping = {
   standard: standardTokenizer,
@@ -32,33 +28,37 @@ class Store {
     this.docKey = docKey;
     this.mapping = mapping;
 
-    this.fieldCountIndex = emptyMap;
-    this.fieldLengthIndex = emptyMap;
+    this.fieldCountIndex = new Map();
+    this.fieldLengthIndex = new Map();
 
-    this.documentIndex = emptyMap;
-    this.idTokensIndex = mappingFields.reduce((acc, field) => acc.set(field, emptyMap), emptyMap);
-    this.tokenToIdsIndex = mappingFields.reduce((acc, field) => acc.set(field, emptyMap), emptyMap);
+    this.documentIndex = new Map();
+    this.docIdTokensIndex = mappingFields.reduce((acc, field) => (
+      acc.set(field, new Map())
+    ), new Map());
+    this.tokenDocIdsIndex = mappingFields.reduce((acc, field) => (
+      acc.set(field, new Map())
+    ), new Map());
   }
 
   add(document = {}) {
-    const { docKey, mapping, documentIndex, tokenToIdsIndex } = this;
+    const { docKey, mapping, documentIndex, tokenDocIdsIndex } = this;
     const { [docKey]: id } = document;
     this.documentIndex = documentIndex.set(id, document);
 
-    this.tokenToIdsIndex = tokenToIdsIndex.map((fieldValue, field) => {
+    this.tokenDocIdsIndex = tokenDocIdsIndex.map((fieldValue, field) => {
       const { [field]: value = '' } = document;
       const { [field]: { analyzer: analyzerName = 'standard' } = {} } = mapping;
       const docTerms = getAnalyzer(analyzerName)(`${value}`);
       const termLength = docTerms.length;
 
-      this.idTokensIndex = this.idTokensIndex.setIn([field, id], docTerms);
+      this.docIdTokensIndex = this.docIdTokensIndex.setIn([field, id], docTerms);
       this.fieldLengthIndex = this.fieldLengthIndex.update(field, 0, length => length + termLength);
       this.fieldCountIndex = this.fieldCountIndex.update(field, 0, count => (
         count + (termLength ? 1 : 0)
       ));
 
       return docTerms.reduce((acc, term) => (
-        acc.update(term, emptySet, listIds => listIds.add(id))
+        acc.update(term, new Set(), listIds => listIds.add(id))
       ), fieldValue);
     });
   }
@@ -67,37 +67,38 @@ class Store {
     const {
       mapping,
       documentIndex,
-      idTokensIndex,
       fieldCountIndex,
       fieldLengthIndex,
-      tokenToIdsIndex,
+      docIdTokensIndex,
+      tokenDocIdsIndex,
     } = this;
-    const documentIds = tokenToIdsIndex.reduce((acc, fieldValue, field) => {
+
+    const documentIds = tokenDocIdsIndex.reduce((acc, fieldValue, field) => {
       const { [field]: { analyzer = 'standard' } = {} } = mapping;
       const docFieldCount = fieldCountIndex.get(field, 1);
       const docFieldLength = fieldLengthIndex.get(field, 0);
       const docTerms = getAnalyzer(analyzer)(`${keyword}`);
 
       const scoreDocs = docTerms.reduce((accum, term) => {
-        const idsTerm = fieldValue.get(term, emptySet);
+        const idsTerm = fieldValue.get(term, new Set());
         const docFieldFreq = idsTerm.count();
         const avgdl = docFieldLength / docFieldCount;
         const idf = Math.log(1 + (docFieldCount - docFieldFreq + 0.5) / (docFieldFreq + 0.5));
 
         const scores = idsTerm
           .filter((docId) => { // operator=and
-            const tokens = idTokensIndex.getIn([field, docId]);
+            const tokens = docIdTokensIndex.getIn([field, docId]);
             return docTerms.every(docTerm => tokens.includes(docTerm));
           })
           .reduce((accDoc, docId) => {
-            const tokens = idTokensIndex.getIn([field, docId]);
+            const tokens = docIdTokensIndex.getIn([field, docId]);
             const dl = tokens.length;
 
             const freq = tokens.filter(token => token === term).length;
             const score = idf * freq * (K1 + 1) / (freq + K1 * (1 - B + B * dl / avgdl));
 
             return accDoc.set(docId, score);
-          }, emptyMap);
+          }, new Map());
 
         return scores.reduce((accScore, score, docId) => (
           accScore.update(docId, 0, currentScore => currentScore + score)
@@ -105,7 +106,7 @@ class Store {
       }, acc);
 
       return scoreDocs;
-    }, emptyMap);
+    }, new Map());
 
     return documentIds
       .sort((a, b) => b - a)
