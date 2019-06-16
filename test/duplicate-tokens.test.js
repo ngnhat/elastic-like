8dc986935a8a5c83d2bddc7a443f846a1c1674b5
nhat ng <ngnhatjs@gmail.com>
2019-06-16 23:01:54 +0700

    Write test

---
+++ test/duplicate-tokens.test.js
@@ -0,0 +1,26 @@
+/**
+ * Created by ngnhat on Sat May 25 2019
+ */
+const Store = require('../index');
+
+const store = new Store();
+
+store.add({ Id: 1, Code: 'code001', Name: 'buổi trưa ăn bưởi chua' });
+
+describe('duplicates', () => {
+  it('chua chua', () => {
+    expect(store.search('chua chua')).toMatchSnapshot();
+  });
+
+  it('bưởi trưa ăn bưởi chua', () => {
+    expect(store.search('bưởi trưa ăn bưởi chua')).toMatchSnapshot();
+  });
+
+  it('buổi trưa ăn buổi chua', () => {
+    expect(store.search('buổi trưa ăn buổi chua')).toMatchSnapshot();
+  });
+
+  it('buổi trưa ăn bưởi chua', () => {
+    expect(store.search('buổi trưa ăn bưởi chua')).toMatchSnapshot();
+  });
+});
